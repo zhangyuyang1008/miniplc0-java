84e955659b0663c19881fd2c02a59d5b93e23918
zhangyuyang1008 <zhangyuyang1008@buaa.edu.cn>
2020-10-26 15:07:02 +0800

    x

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -294,7 +294,7 @@ public final class Analyser {
     private void analyseStatementSequence() throws CompileError {
         // 语句序列 -> 语句*
         // 语句 -> 赋值语句 | 输出语句 | 空语句
-        if(check(TokenType.Ident) || check(TokenType.Print) || check(TokenType.None)) {
+        if(check(TokenType.Ident) || check(TokenType.Print) || check(TokenType.Semicolon)) {
             while (true) {
                 // 如果下一个 token 是……
                 var peeked = peek();
